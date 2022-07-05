91b5bad24bc9bb7f73fadc80fc6a06d421440b06
Harsha Vardhan <charagon@usc.edu>
2022-07-04 23:30:11 -0700

    Models

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,3 +1,7 @@
 export const getPosts = ( req, res) =>{
     res.send("Working through controller bro");
 }
+
+export const createPosts = (req, res) =>{
+    res.send("Post creation bro ");
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,8 @@
 import Express from "express";
-import { getPosts } from "../controllers/postController.js";
+import { getPosts, createPosts } from "../controllers/postController.js";
 const router_var = Express.Router();
 
 router_var.get('/', getPosts )
-
+router_var.post('/', createPosts )
 router_var.get('/', );
 export default router_var;
