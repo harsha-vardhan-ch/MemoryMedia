d0a2e879b2fe007d6778efcb9fa3c6f01218a3fa
Harsha Vardhan <charagon@usc.edu>
2022-12-23 15:02:59 -0800

    Auth Middleware and Routing modification
    Modified routes so that middleware auth is called before CRUD operations.

---
+++ server/middleware/authMiddle.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+const auth = async(req,res,next) => {
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+
+        // Deciding if token is custom token or third party token through length.
+        // False = Google Token, True = Manual token
+        const isCustomAuth = token.length<500;
+
+        let decodedData;
+        // For manual login
+        if(token && isCustomAuth){
+            // Verifyinh token with the secret phrase used for creating token.
+            decodedData = jwt.verify(token ,'test');
+            req.userID = decodedData?.id;
+        }
+        // For 3rd party google login
+        else{
+            decodedData = jwt.verify(token);
+            req.userID = decodedData?.sub;   // userID provided through sub variable in Google Auth
+        }
+        next();
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export default auth;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,20 @@
 import Express from "express";
-import { getPosts, createPosts, updatePosts, deletePosts, likePosts, getPost } from "../controllers/postController.js";
+import {
+	getPosts,
+	createPosts,
+	updatePosts,
+	deletePosts,
+	likePosts,
+	getPost,
+} from "../controllers/postController.js";
+import auth from "../middleware/authMiddle.js";
 const router_var = Express.Router();
 
 router_var.get("/", getPosts);
-router_var.post("/", createPosts);
-router_var.patch("/:id", updatePosts);
-router_var.delete("/:id", deletePosts);
-router_var.patch("/:id/likePost", likePosts);
-router_var.get("/");
-router_var.get("/:id", getPost);
+router_var.post("/", auth, createPosts);
+router_var.patch("/:id", auth, updatePosts);
+router_var.delete("/:id", auth, deletePosts);
+router_var.patch("/:id/likePost", auth, likePosts);
+// router_var.get("/");
+// router_var.get("/:id", getPost);
 export default router_var;
